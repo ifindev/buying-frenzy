6d63d85e0a502a04aba5b0efa5fa9ca27401a61f
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-03-29 17:03:01 +0700

    feat : Added new error message

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -1,5 +1,6 @@
 export const errMessages = {
 	ErrConvertTimeToMinutes: 'Error converting time to minutes. Time should be in HH:MM format!',
 	ErrConvertMinutesToTime:
-		'Error converting minutes to HH:MM time. Minutes value should be less than 1440'
+		'Error converting minutes to HH:MM time. Minutes value should be less than 1440',
+	ErrConvertTimeFromAMPM: 'Error! Parameter time should includes AM/am or PM/pm identification'
 }
