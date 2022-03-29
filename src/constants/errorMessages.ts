e7d311fbee9c2ab7265b477d18c0e46d12cecfe4
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-03-29 17:21:54 +0700

    feat : Added new error message for hour error

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -2,5 +2,6 @@ export const errMessages = {
 	ErrConvertTimeToMinutes: 'Error converting time to minutes. Time should be in HH:MM format!',
 	ErrConvertMinutesToTime:
 		'Error converting minutes to HH:MM time. Minutes value should be less than 1440',
-	ErrConvertTimeFromAMPM: 'Error! Parameter time should includes AM/am or PM/pm identification'
+	ErrConvertTimeFromAMPM: 'Error! Parameter time should includes AM/am or PM/pm identification',
+	ErrInputHourLargerThan12: 'Valid input hour must be between 0-12!'
 }
