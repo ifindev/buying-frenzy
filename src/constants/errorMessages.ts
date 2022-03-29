6532b800a988787478df0fbf0b597cf016378b73
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-03-29 15:44:32 +0700

    feat : Added new error message

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -1,3 +1,5 @@
 export const errMessages = {
-	ConvertTimeToMinutesErr: 'Error converting time to minutes. Time should be in HH:MM format!'
+	ErrConvertTimeToMinutes: 'Error converting time to minutes. Time should be in HH:MM format!',
+	ErrConvertMinutesToTime:
+		'Error converting minutes to HH:MM time. Minutes value should be less than 1440'
 }
