9c4c2139642cd60693faf4c5ccf8de825fa8a59d
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-03-29 15:45:27 +0700

    refactor: refactor timeProcess module into service

--- __tests__/services/timeProcess/timeProcess.ts
+++ __tests__/services/timeProcess/timeProcess.ts
@@ -1,6 +1,5 @@
 import { expect } from 'chai'
-import { errMessages } from '../../src/constants/errorMessages'
-import { cvtTimeToMinutes, cvtMinutesToTime } from '../../src/db/etl/modules/timeProcess'
+import { cvtTimeToMinutes, cvtMinutesToTime } from '../../../src/services/time/timeProcess'
 
 describe('Time Processor Modules', () => {
 	describe('Convert time of 24-hours HH:MM to integer minutes past midnight', () => {
@@ -51,21 +50,31 @@ describe('Time Processor Modules', () => {
 			const input1 = 60
 			const input2 = 105
 			const input3 = 1223
+			const input4 = 933
 
 			// Expected output
 			const output1 = '01:00'
 			const output2 = '01:45'
 			const output3 = '20:23'
+			const output4 = '15:33'
 
 			// Exec functions
 			const res1 = cvtMinutesToTime(input1)
 			const res2 = cvtMinutesToTime(input2)
 			const res3 = cvtMinutesToTime(input3)
+			const res4 = cvtMinutesToTime(input4)
 
 			// Run tests
 			expect(res1).to.equal(output1)
 			expect(res2).to.equal(output2)
 			expect(res3).to.equal(output3)
+			expect(res4).to.equal(output4)
+			done()
+		})
+
+		it('Should throw an error if minutes to be converted is >= 1440 minutes', (done) => {
+			const input = 1440
+			expect(() => cvtMinutesToTime(input)).to.throw()
 			done()
 		})
 	})
---
+++ src/services/time/timeProcess.ts
@@ -0,0 +1,51 @@
+import { errMessages } from '../../constants/errorMessages'
+
+/**
+ * Function to convert HH:MM (24-h) time
+ * into total minutes after midnight
+ *
+ * @param time (string): time in 24-hours base with HH:MM format
+ * @returns timeMinutes (number) : time in minutes past midnight format
+ */
+function cvtTimeToMinutes(time: string): number {
+	// Check Data
+	const check = time.includes(':') && time.length > 2
+	if (!check) {
+		throw new Error(`${errMessages.ErrConvertTimeToMinutes}`)
+	}
+
+	// Split time to hours and minutes
+	const splitTime = time.split(':')
+	const hours = parseInt(splitTime[0])
+	const minutes = parseInt(splitTime[1])
+
+	// Calculate total minutes past midnight
+	const timeMinutes = hours * 60 + minutes
+
+	// return
+	return timeMinutes
+}
+
+/**
+ * Function to convert time from minutes past
+ * midnight format into 24-h HH:MM
+ *
+ * @param timeMinutes (number)
+ * @returns hhmm : Time in 24-h HH:MM format
+ */
+function cvtMinutesToTime(timeMinutes: number): string {
+	if (timeMinutes >= 1440) {
+		throw new Error(errMessages.ErrConvertMinutesToTime)
+	}
+
+	const hours = Math.floor(timeMinutes / 60)
+	const minutes = timeMinutes % 60
+
+	const hoursTime = hours < 10 ? `0${hours}` : `${hours}`
+	const minutesTime = minutes === 0 ? '00' : `${minutes}`
+
+	const hhmm = `${hoursTime}:${minutesTime}`
+	return hhmm
+}
+
+export { cvtTimeToMinutes, cvtMinutesToTime }
