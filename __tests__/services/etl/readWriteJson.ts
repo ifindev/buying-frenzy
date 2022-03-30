afb6ca565cabc837eb445c8af4f3cba98e163678
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-03-30 13:30:58 +0700

    test: Setup tests for readWriteJson service module

---
+++ __tests__/services/etl/readWriteJson.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai'
+import { readJsonFile, writeJsonFile } from '../../../src/services/etl/readWriteJson'
+
+describe('Test read JSON File from a given directory using readJsonFile', () => {
+	it('Return json data successfully if file exists in the file directory', (done) => {
+		done()
+	})
+
+	it('Return error if file is not exists in the file directory', (done) => {
+		done()
+	})
+})
+
+describe('Test write JSON data with a given filename to the target directory using writeJsonFile', () => {
+	it('Should return error if file to be write is not .json', (done) => {
+		done()
+	})
+
+	it('Should write write file successfully to the given filedir', (done) => {
+		done()
+	})
+})
