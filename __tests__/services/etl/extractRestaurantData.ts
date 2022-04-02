76d594c0c4309621fdca9d30b63e3b4d7bd113c8
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-04-02 16:34:51 +0700

    test : Added test for extractWorkingHoursData func

--- __tests__/services/etl/extractRestaurantData.ts
+++ __tests__/services/etl/extractRestaurantData.ts
@@ -105,4 +105,38 @@ describe('Test the function used to normalize and extract restaurant working hou
 			done()
 		})
 	})
+
+	describe('Get restaurant working hours data for the entire week', () => {
+		const restoId = 1
+		const OH1 =
+			'Mon, Fri 2:30 pm - 8 pm / Tues 11 am - 2 pm / Weds 1:15 pm - 3:15 am / Thurs 10 am - 3:15 am / Sat 5 am - 11:30 am / Sun 10:45 am - 5 pm'
+		const OH2 =
+			'Mon, Weds 11:45 am - 4:45 pm / Tues 7:45 am - 2 am / Thurs 5:45 pm - 12 am / Fri, Sun 6 am - 9 pm / Sat 10:15 am - 9 pm'
+
+		const out1 = extractWorkingHoursData(restoId, OH1)
+		const out2 = extractWorkingHoursData(restoId, OH2)
+
+		it('Should return restaurant working hours data in an array', (done) => {
+			expect(out1).to.be.an('array')
+			expect(out2).to.be.an('array')
+
+			done()
+		})
+
+		it('Should return array of length seven for a week opening hours data', (done) => {
+			expect(out1).to.have.lengthOf(7)
+			expect(out2).to.have.lengthOf(7)
+
+			done()
+		})
+
+		it('Should return an array with IWorkingHours data as its element', (done) => {
+			const elm = out1[0]
+			expect(elm).to.have.property('restaurantId')
+			expect(elm).to.have.property('dayOfWeek')
+			expect(elm).to.have.property('openingHour')
+			expect(elm).to.have.property('closingHour')
+			done()
+		})
+	})
 })
