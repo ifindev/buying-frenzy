1b4d747fd156641cd5e9088265c548bd33962c92
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-04-07 15:01:30 +0700

    chore: Update config params

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -4,48 +4,56 @@ import * as dotenv from 'dotenv'
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const envFound = dotenv.config({ path: `${__dirname}/../../.env` })
-if(envFound.error) {
-  // Crash the whole process
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+if (envFound.error) {
+	// Crash the whole process
+	throw new Error("⚠️  Couldn't find .env file  ⚠️")
 }
 
 export namespace Database {
-  export const schema = 'api'
-  export const url= process.env.DB_URL
-  export const host = process.env.DB_HOST
-  export const user = process.env.DB_USER
-  export const database = process.env.DATABASE
-  export const password = process.env.DB_PASSWORD
-  export const minPoolSize = parseInt(process.env.DB_MIN_POOL_SIZE!)
-  export const maxPoolSize = parseInt(process.env.DB_MAX_POOL_SIZE!)
-  export const port = parseInt(process.env.DB_PORT!, 10)
+	export const schema = 'api'
+	export const url = process.env.DB_URL
+	export const host = process.env.DB_HOST
+	export const user = process.env.DB_USER
+	export const database = process.env.DATABASE
+	export const password = process.env.DB_PASSWORD
+	export const minPoolSize = parseInt(process.env.DB_MIN_POOL_SIZE!)
+	export const maxPoolSize = parseInt(process.env.DB_MAX_POOL_SIZE!)
+	export const port = parseInt(process.env.DB_PORT!, 10)
 }
 
 export namespace Server {
-   export const port = parseInt(process.env.PORT || '1337', 10)
-   export const isDev = process.env.NODE_ENV === 'development'
+	export const port =
+		process.env.NODE_ENV === 'docker' ? 1337 : parseInt(process.env.PORT || '3001', 10)
+	export const isDev = process.env.NODE_ENV === 'development'
 }
 
 export namespace KnexCfg {
-  export const schema = 'resto'
+	export const schema = 'resto'
 
-  export const config = {
-    client: 'postgresql',
-    connection: {
-      host: Database.host,
-      database: Database.database,
-      user: Database.user,
-      password: Database.password,
-      port: Database.port
-    },
-    pool: {
-      min: Database.minPoolSize,
-      max: Database.maxPoolSize
-    },
-    migrations: {
-      tableName: 'KnexMigrations'
-    }
-  }
+	export const config = {
+		client: 'postgresql',
+		connection: {
+			host: Database.host,
+			database: Database.database,
+			user: Database.user,
+			password: Database.password,
+			port: Database.port
+		},
+		pool: {
+			min: Database.minPoolSize,
+			max: Database.maxPoolSize
+		},
+		migrations: {
+			tableName: 'KnexMigrations'
+		},
+		seeds: {
+			directory: './seeds'
+		}
+	}
 }
 
-export default {Database, Server, KnexCfg}
+export namespace ApiVersion {
+	export const v1 = '/api/v1'
+}
+
+export default { Database, Server, KnexCfg, ApiVersion }
