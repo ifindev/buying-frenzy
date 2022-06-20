2b28104d2d1df4e7cebaad8ceee12c5b17d50af7
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-06-20 14:46:52 +0700

    feat: Add repo resto with dish within price range

--- src/repositories/restaurants.ts
+++ src/repositories/restaurants.ts
@@ -58,4 +58,46 @@ const getCountOpenRestaurants = async (
 	}
 }
 
-export { findOpenRestaurants, getCountOpenRestaurants }
+const getRestaurantWithDishesWithinPriceRange = async (
+	minPrice: number,
+	maxPrice: number,
+	numDishes: number,
+	numRestaurants: number
+) => {
+	try {
+		// subquery to get count restaurant with dish within the specified price range
+		const countResto = db(`${Database.schema}.Restaurant`)
+			.join(
+				`${Database.schema}.RestaurantMenu`,
+				'Restaurant.restaurantId',
+				'=',
+				'RestaurantMenu.restaurantId'
+			)
+			.select('Restaurant.restaurantId')
+			.count('Restaurant.restaurantId as countResto')
+			.where('RestaurantMenu.price', '>=', minPrice)
+			.andWhere('RestaurantMenu.price', '<=', maxPrice)
+			.groupBy('Restaurant.restaurantId')
+			.as('RestoCountWithPriceRange')
+
+		// subquery to get restaurants with at least `numDishes` of dish
+		// within the specified price range
+		const getRestoID = db
+			.select('restaurantId')
+			.from(countResto)
+			.where('countResto', '>=', numDishes)
+
+		// get restaurant id & names
+		const getRestoInfo = await db
+			.select('Restaurant.restaurantId', 'Restaurant.restaurantName')
+			.from(`${Database.schema}.Restaurant`)
+			.where('Restaurant.restaurantId', 'in', getRestoID)
+			.limit(numRestaurants)
+
+		return getRestoInfo
+	} catch (err) {
+		throw new Error('Error fetching data from DB')
+	}
+}
+
+export { findOpenRestaurants, getCountOpenRestaurants, getRestaurantWithDishesWithinPriceRange }
